21769240354d141d9dd4522eb189f5bd35f9c8f1
Lyndon Maydwell <lyndon@sordina.net>
2014-11-22 21:05:54 +1100

    Update lights when adding commands

--- src/scriptMidi.hs
+++ src/scriptMidi.hs
@@ -12,30 +12,44 @@ main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   sources <- enumerateSources
+  dests   <- enumerateDestinations
   names   <- mapM getName sources
+  dNames  <- mapM getName dests
+
   comment "What source do you want?"
   mapM_ (comment . \(a,b) -> show a ++ " - " ++ b) (zip [1 :: Int ..] names)
+
   choiceID  <- readMay `fmap` getLine :: IO (Maybe Int)
-  run choiceID $ do cid <- choiceID
-                    lookup cid (zip [1..] sources)
 
-run :: Maybe Int -> Maybe Source -> IO ()
-run (Just choiceID) (Just choice) = print choiceID           >> setup choice
-run _                _            = comment "Invalid Option" >> main
+  run $ do cid    <- choiceID
+           name   <- lookup cid  (zip [1..]  names)
+           source <- lookup name (zip names  sources)
+           return (cid, source, lookup name (zip dNames dests))
 
-setup :: Source -> IO ()
-setup choice = do
+run :: Maybe (Int, Source, Maybe Destination) -> IO ()
+run (Just (choiceID, source, dest)) = print choiceID           >> setup source dest
+run _                               = comment "Invalid Option" >> main
+
+maybeOpenDestination :: Maybe Destination -> IO (Maybe Connection)
+maybeOpenDestination Nothing     = return Nothing
+maybeOpenDestination (Just dest) = do conn <- openDestination dest
+                                      start  conn
+                                      return (Just conn)
+
+setup :: Source -> Maybe Destination -> IO ()
+setup source dest = do
   buttonIDs     <- newChan
   buttonStrings <- mapMChan createButton buttonIDs
   keys          <- newChan
   events        <- mergeChans buttonStrings keys
-  conn          <- openSource choice (Just (maybeWriteChan buttonIDs . getID))
-  start conn
+  sConn         <- openSource source (Just (maybeWriteChan buttonIDs . getID))
+  dConn         <- maybeOpenDestination dest
+  start sConn
   comment "Processing Events"
   void $ forkIO $ do getContents >>= writeList2Chan keys . map Keyboard . lines
                      writeChan keys (Keyboard "exit")
-  updateCommands events
-  stop conn
+  respondToEvents events dConn
+  stop sConn
 
 getID :: MidiEvent -> Maybe Int
 getID (MidiEvent _ (MidiMessage _ (NoteOn x _))) = Just x
@@ -66,21 +80,32 @@ mergeChans a b = do
   void $ forkIO $ getChanContents b' >>= writeList2Chan c
   return c
 
-respondCommand :: Command -> IO ()
--- respondCommand (Run s  ) = void $ forkIO $ callCommand s
-respondCommand (Run s  ) = void $ forkIO $ void $ createProcess
-                                                  ((shell s)
-                                                   { delegate_ctlc = True
-                                                   , std_out       = UseHandle stderr })
-respondCommand (Log a b) = putStrLn a >> putStrLn b
+respondCommand :: Maybe Connection -> Command -> IO ()
+respondCommand _    (Run s  ) = void $ forkIO $ void $ createProcess
+                                                       ((shell s)
+                                                        { delegate_ctlc = True
+                                                        , std_out       = UseHandle stderr })
+respondCommand dest (Log a b) = do putStrLn a
+                                   putStrLn b
+                                   case dest of Nothing -> return ()
+                                                -- When a binding is created, turn on the light for that binding
+                                                -- Just d  -> do send d $ MidiMessage 1 $ NoteOn (read a - 67) 60
+                                                Just d  -> lightUp d (read a)
 
-updateCommands :: Chan Event -> IO ()
-updateCommands events = getChanContents events
-                    >>= mapM_           respondCommand
-                      . behave          []
-                      . filter          (not . isComment)
-                      . filter          (/= Keyboard "")
-                      . takeWhile       (/= Keyboard "exit")
+lightUp :: Connection -> Int -> IO ()
+lightUp conn n | n >= 68 || n <= 71   = send conn $ MidiMessage 1 $ NoteOn (n - 67) 60
+               | n >= 72 || n <= 75   = send conn $ MidiMessage 1 $ NoteOn (n - 63) 60
+               | n >= 76 || n <= 79   = send conn $ MidiMessage 1 $ NoteOn (n - 59) 60
+               | n >= 80 || n <= 83   = send conn $ MidiMessage 1 $ NoteOn (n - 55) 60
+               | otherwise            = return ()
+
+respondToEvents :: Chan Event -> Maybe Connection -> IO ()
+respondToEvents events dest = getChanContents events
+                          >>= mapM_           (respondCommand dest)
+                            . behave          []
+                            . filter          (not . isComment)
+                            . filter          (/= Keyboard "")
+                            . takeWhile       (/= Keyboard "exit")
 
 isComment :: Event -> Bool
 isComment (Keyboard x) = and $ zipWith (==) x "#"
